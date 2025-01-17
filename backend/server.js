9aab84fc6e334e25bd9ba7070f96091859472acb
John Thuku <ceogachone@gmail.com>
2025-01-17 22:12:39 +0300

    backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); // to accept json data
+
+app.use("/api/products", productRoutes);
+
+app.listen(5000, () => {
+    connectDB();
+    console.log('server started at http://localhost:5000');
+})
